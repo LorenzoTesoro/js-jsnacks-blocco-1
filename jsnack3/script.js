654f67cc9cbf942c65aaa1ab0e77c5ec072a30f1
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-12 12:43:42 +0200

    initial commit

---
+++ jsnack3/script.js
@@ -0,0 +1,12 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+// 1- Il software deve chiedere per 10 volte all’utente di inserire un numero. - ciclo for
+
+for (let i = 0; i < 10; i++){
+    let number = Number(prompt("inserisci un numero"));
+    number[i] = number
+    console.log(number);
+}
+
+// 2 - Il programma stampa la somma di tutti i numeri inseriti. 
